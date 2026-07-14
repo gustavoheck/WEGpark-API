5de09056c4c296a93b39b3016faeedac7b6bb396
Gustavo Henrique Heck <gustavohenriqueheck@gmail.com>
2026-07-14 17:37:43 -0300

    feat: first commit

---
+++ src/main/java/com/weg/WEGpark/WeGparkApplication.java
@@ -0,0 +1,13 @@
+package com.weg.WEGpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeGparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeGparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weg/WEGpark/WeGparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.WEGpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeGparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
