6748675ffbcd3d53b505ea31cfda08be6bf2c020
GUSTAVO HENRIQUE HECK <gustavo_h_heck@estudante.sesisenai.org.br>
2026-07-16 10:24:10 -0300

    feat: creating controller for vehicle register

---
+++ src/main/java/com/weg/WEGpark/park/internal/controller/vehicle/VehicleController.java
@@ -0,0 +1,28 @@
+package com.weg.WEGpark.park.internal.controller.vehicle;
+
+import com.weg.WEGpark.park.internal.app.vehicle.service.VehicleService;
+import com.weg.WEGpark.park.internal.dto.vehicle.CreateVehicleRequestDTO;
+import com.weg.WEGpark.park.internal.dto.vehicle.CreateVehicleResponseDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/vehicle")
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    public ResponseEntity<CreateVehicleResponseDTO> registerVehicle (@Valid @RequestBody CreateVehicleRequestDTO request) {
+        CreateVehicleResponseDTO response = vehicleService.registerVehicle(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
