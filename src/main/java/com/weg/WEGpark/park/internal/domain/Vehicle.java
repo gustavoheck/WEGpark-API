d7180cfb7b11ffcbc83057ff9f6c3f91a8504262
GUSTAVO HENRIQUE HECK <gustavo_h_heck@estudante.sesisenai.org.br>
2026-07-16 08:45:37 -0300

    feat: creating vehicle model

--- src/main/java/com/weg/WEGpark/park/internal/domain/Vehicle.java
+++ src/main/java/com/weg/WEGpark/park/internal/domain/Vehicle.java
@@ -1,4 +1,35 @@
 package com.weg.WEGpark.park.internal.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(schema = "park", name = "vehicle")
 public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String model;
+
+    @Column(nullable = false)
+    String brand;
+
+    @Column(nullable = false)
+    String color;
+
+    public Vehicle(String model, String brand, String color) {
+        this.model = model;
+        this.brand = brand;
+        this.color = color;
+    }
 }
