ded40a54316f81a823307135044f09763493fd45
Gustavo Henrique Heck <gustavohenriqueheck@gmail.com>
2026-07-19 17:27:42 -0300

    fix: applying mapstruct into ocurrence mappers

--- src/main/java/com/weg/WEGpark/park/internal/app/occurrence/mapper/OccurrenceMapper.java
+++ src/main/java/com/weg/WEGpark/park/internal/app/occurrence/mapper/OccurrenceMapper.java
@@ -3,25 +3,14 @@ package com.weg.WEGpark.park.internal.app.occurrence.mapper;
 import com.weg.WEGpark.park.internal.domain.model.occurrence.Occurrence;
 import com.weg.WEGpark.park.internal.dto.occurrence.OccurrenceRequestDto;
 import com.weg.WEGpark.park.internal.dto.occurrence.OccurrenceResponseDto;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
-@Component
-public class OccurrenceMapper {
+@Mapper(componentModel = "spring")
+public interface OccurrenceMapper {
 
-    public Occurrence toEntity(OccurrenceRequestDto occurrenceRequestDto) {
-        return new Occurrence(
-                occurrenceRequestDto.location(),
-                occurrenceRequestDto.gate()
-        );
-    }
+    public Occurrence toEntity(OccurrenceRequestDto occurrenceRequestDto);
 
-    public OccurrenceResponseDto toResponse(Occurrence occurrence) {
-        return new OccurrenceResponseDto(
-                occurrence.getId(),
-                occurrence.getDateHour(),
-                occurrence.getLocation(),
-                occurrence.getGate()
-        );
-    }
+    public OccurrenceResponseDto toResponse(Occurrence occurrence);
 }
