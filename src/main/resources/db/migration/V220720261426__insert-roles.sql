272ab80be2cb71bdc242d5e23879b430bacbb304
gustavo_h_heck <gustavo_h_heck@estudante.sesisenai.org.br>
2026-07-22 14:29:18 -0300

    feat: creating flyway migration for inserting roles default values

---
+++ src/main/resources/db/migration/V220720261426__insert-roles.sql
@@ -0,0 +1,19 @@
+INSERT INTO auth.role
+    (role)
+VALUES
+    ('GUARD');
+
+INSERT INTO auth.role
+    (role)
+VALUES
+    ('RH');
+
+INSERT INTO auth.role
+    (role)
+VALUES
+    ('ADMIN');
+
+INSERT INTO auth.role
+    (role)
+VALUES
+    ('PARK');
