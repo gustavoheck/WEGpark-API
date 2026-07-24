0bc2fcde7c551964884d441fcf12fc43a4693826
Gustavo Henrique Heck <gustavohenriqueheck@gmail.com>
2026-07-23 21:14:07 -0300

    feat: creating vehicle user disassociate

---
+++ src/main/java/com/weg/WEGpark/park/internal/infra/repository/VehicleUserRepository.java
@@ -0,0 +1,15 @@
+package com.weg.WEGpark.park.internal.infra.repository;
+
+import com.weg.WEGpark.park.internal.domain.model.users.ParkUser;
+import com.weg.WEGpark.park.internal.domain.model.users.VehicleUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface VehicleUserRepository extends JpaRepository<VehicleUser, Long> {
+
+    Optional<VehicleUser> findByVehicleUuid (UUID uuid);
+}
