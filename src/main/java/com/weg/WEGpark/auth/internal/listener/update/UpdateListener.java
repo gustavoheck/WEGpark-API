77a10ee855539f36a0a111aa0479b17fd7311349
Gustavo Henrique Heck <gustavohenriqueheck@gmail.com>
2026-07-27 18:41:26 -0300

    feat: creating auth update user listener"

---
+++ src/main/java/com/weg/WEGpark/auth/internal/listener/update/UpdateListener.java
@@ -0,0 +1,22 @@
+package com.weg.WEGpark.auth.internal.listener.update;
+
+import com.weg.WEGpark.auth.UpdateUserAuthEvent;
+import com.weg.WEGpark.auth.internal.app.service.UpdateService;
+import com.weg.WEGpark.rh.RegisterRhEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class UpdateListener {
+
+    private final UpdateService updateService;
+
+    @EventListener
+    public void updateUser (UpdateUserAuthEvent event, UUID userUuid) {
+        updateService.updateUserAuthDataEvent(event, userUuid);
+    }
+}
