72bf2c2cec9261fc3c212d70b811d91175d0f550
Gustavo Henrique Heck <gustavohenriqueheck@gmail.com>
2026-07-28 21:50:57 -0300

    fix: fixing rh operation fk creation

--- src/main/resources/db/migration/V270720261009__creating-rh-operation-fk.sql
+++ src/main/resources/db/migration/V270720261009__creating-rh-operation-fk.sql
@@ -1,12 +1,11 @@
-
-ALTER TABLE auth.users
+ALTER TABLE rh.operation
 ADD CONSTRAINT fk_operated_user
-FOREIGN KEY (id_operated_user) REFERENCES auth.users(id),
+FOREIGN KEY (id_operated_user) REFERENCES auth.users(id);
 
 ALTER TABLE rh.operation
 ADD CONSTRAINT fk_rh
-FOREIGN KEY (id_rh) REFERENCES rh.rh(id)
+FOREIGN KEY (id_rh) REFERENCES rh.rh(id);
 
 ALTER TABLE rh.rh
 ADD CONSTRAINT fk_operation
-FOREIGN KEY (id_operation) REFERENCES rh.operation(id)
+FOREIGN KEY (id_operation) REFERENCES rh.operation(id);
