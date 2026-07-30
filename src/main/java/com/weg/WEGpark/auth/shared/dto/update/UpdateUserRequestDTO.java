d7fba16ab79e700fdf19f8b02fc601b952b23c62
GUSTAVO HENRIQUE HECK <gustavo_h_heck@estudante.sesisenai.org.br>
2026-07-30 10:43:28 -0300

    fix: not validating new password on update password

--- src/main/java/com/weg/WEGpark/auth/shared/dto/update/UpdateUserRequestDTO.java
+++ src/main/java/com/weg/WEGpark/auth/shared/dto/update/UpdateUserRequestDTO.java
@@ -1,11 +1,14 @@
 package com.weg.WEGpark.auth.shared.dto.update;
 
+import com.weg.WEGpark.auth.internal.dto.validation.ValidPassword;
+
 public record UpdateUserRequestDTO(
 
         String email,
 
         String role,
 
+        @ValidPassword
         String password,
 
         String actualPassword,
