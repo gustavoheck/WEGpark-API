1f39c7fef357a9812f1483802698d03871d04e0f
Gustavo Henrique Heck <gustavohenriqueheck@gmail.com>
2026-08-02 22:07:22 -0300

    feat: creating cors configuration

--- src/main/java/com/weg/WEGpark/auth/internal/infra/security/config/SecurityConfig.java
+++ src/main/java/com/weg/WEGpark/auth/internal/infra/security/config/SecurityConfig.java
@@ -42,6 +42,11 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PATCH, "/auth/reset-password").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/reset-password/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/admin").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
