04f933a2580056dbeb7204ec423f690d2bb29f1c
Gustavo Henrique Heck <gustavohenriqueheck@gmail.com>
2026-08-07 00:47:56 -0300

    feat: adding validation for plate at creating vehicle

--- src/main/java/com/weg/WEGpark/park/internal/dto/vehicle/defaults/CreateVehicleRequestDTO.java
+++ src/main/java/com/weg/WEGpark/park/internal/dto/vehicle/defaults/CreateVehicleRequestDTO.java
@@ -1,12 +1,14 @@
 package com.weg.WEGpark.park.internal.dto.vehicle.defaults;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CreateVehicleRequestDTO(
 
         @NotBlank(message = "The plate of vehicle can not be null")
         @Size(min = 7, max = 7, message = "The plate of of a vehicle")
+        @Pattern(regexp = "^[A-Z]{3}[0-9][A-Z][0-9]{2}$", message = "The plate must follow the Mercosul pattern (e.g., ABC1D23)")
         String plate,
 
         @NotBlank(message = "The name of vehicle can not be null or blank")
